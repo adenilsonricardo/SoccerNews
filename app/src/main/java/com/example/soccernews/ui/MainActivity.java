ad5027ab213325f563eb12d987db4d96f5565261
adenilsonricardo <85114978+adenilsonricardo@users.noreply.github.com>
2022-07-15 17:17:49 -0300

    add swipeRefresh

--- app/src/main/java/com/example/soccernews/ui/MainActivity.java
+++ app/src/main/java/com/example/soccernews/ui/MainActivity.java
@@ -1,23 +1,21 @@
-package com.example.soccernews;
+package com.example.soccernews.ui;
 
 import android.os.Bundle;
 
-import com.example.soccernews.data.local.AppDataBase;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.soccernews.R;
+import com.example.soccernews.data.local.SoccerNewsDb;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
-import androidx.room.Room;
 
 import com.example.soccernews.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
-    private AppDataBase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,18 +32,6 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
-
-        this.setupDb();
-    }
-
-    private void setupDb() {
-        db = Room.databaseBuilder(this, AppDataBase.class, "database-name")
-                .allowMainThreadQueries()
-                .build();
-    }
-
-    public AppDataBase getDb(){
-        return db;
     }
 
 }
--- app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
+++ app/src/main/java/com/example/soccernews/ui/news/NewsViewModel.java
@@ -1,10 +1,14 @@
 package com.example.soccernews.ui.news;
 
+import android.os.AsyncTask;
+
+import androidx.annotation.NonNull;
 import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.soccernews.data.SoccerNewsRepository;
 import com.example.soccernews.data.remote.SoccerNewsApi;
 import com.example.soccernews.domain.News;
 
@@ -24,23 +28,16 @@ public class NewsViewModel extends ViewModel {
 
     private final MutableLiveData<List<News>> news = new MutableLiveData<>();
     private final MutableLiveData<State> state = new MutableLiveData<>();
-    private final SoccerNewsApi api;
 
     public NewsViewModel() {
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://adenilsonricardo.github.io/soccer-news-api/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .build();
-        api = retrofit.create(SoccerNewsApi.class);
-
         this.findNews();
     }
 
-    private void findNews() {
+    void findNews() {
         state.setValue(State.DOING);
-        api.getNews().enqueue(new Callback<List<News>>() {
+        SoccerNewsRepository.getInstance().getRemoteApi().getNews().enqueue(new Callback<List<News>>() {
             @Override
-            public void onResponse(Call<List<News>> call, Response<List<News>> response) {
+            public void onResponse(@NonNull Call<List<News>> call, @NonNull Response<List<News>> response) {
                 if (response.isSuccessful()){
                     news.setValue(response.body());
                     state.setValue(State.DONE);
@@ -50,11 +47,15 @@ public class NewsViewModel extends ViewModel {
             }
 
             @Override
-            public void onFailure(Call<List<News>> call, Throwable t) {
-                t.printStackTrace();
+            public void onFailure(@NonNull Call<List<News>> call, Throwable error) {
+                error.printStackTrace();
                 state.setValue(State.ERROR);
             }
         });
+    }
+
+    public void saveNews(News news){
+        AsyncTask.execute(() -> SoccerNewsRepository.getInstance().getLocalDb().newsDao().save(news));
     }
 
     public LiveData<List<News>> getNews() {
